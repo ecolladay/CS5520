b46e35fc6ef4c388e83c4821a08df3d62c6870f1
emilycolladay <ecolladay@gmail.com>
2021-05-27 22:29:54 -0400

    Changed the onClick to update TextView.

--- app/src/main/java/edu/neu/madcourse/numad21su_emilycolladay/LettersClick.java
+++ app/src/main/java/edu/neu/madcourse/numad21su_emilycolladay/LettersClick.java
@@ -3,11 +3,12 @@ package edu.neu.madcourse.numad21su_emilycolladay;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LettersClick extends AppCompatActivity {
+public class LettersClick extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +24,7 @@ public class LettersClick extends AppCompatActivity {
         Button buttonE = findViewById(R.id.button_E);
         Button buttonF = findViewById(R.id.button_F);
 
-        // Add action listeners
+        /** Add action listeners
 
         buttonA.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Pressed: A", Toast.LENGTH_SHORT).show());
 
@@ -36,116 +37,32 @@ public class LettersClick extends AppCompatActivity {
         buttonE.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Pressed: E", Toast.LENGTH_SHORT).show());
 
         buttonF.setOnClickListener(v -> Toast.makeText(getApplicationContext(), "Pressed: F", Toast.LENGTH_SHORT).show());
-
-
-
-        /**
-         Button buttonA = new Button(this);
-         buttonA.setText(R.string.A);
-         buttonA.setId(R.id.button_A);
-
-         Button buttonB = new Button(this);
-         buttonB.setText(R.string.B);
-         buttonB.setId(R.id.button_B);
-
-         Button buttonC = new Button(this);
-         buttonC.setText(R.string.C);
-         buttonC.setId(R.id.button_C);
-
-         Button buttonD = new Button(this);
-         buttonD.setText(R.string.D);
-         buttonD.setId(R.id.button_D);
-
-         Button buttonE = new Button(this);
-         buttonE.setText(R.string.E);
-         buttonE.setId(R.id.button_E);
-
-         Button buttonF = new Button(this);
-         buttonF.setText(R.string.F);
-         buttonF.setId(R.id.button_F);
-         */
-
-
-        /**
-        ConstraintLayout myCl = new ConstraintLayout(this);
-
-        myCl.addView(buttonA);
-        myCl.addView(buttonB);
-        myCl.addView(buttonC);
-        myCl.addView(buttonD);
-        myCl.addView(buttonE);
-        myCl.addView(buttonF);
-
-        //setContentView(myCl);
-        setContentView(R.layout.activity_button_letters); // confused here
-
-        ConstraintSet csButtonA = new ConstraintSet();
-
-        csButtonA.constrainHeight(buttonA.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonA.constrainWidth(buttonA.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonA.connect(buttonA.getId(), ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 0);
-        csButtonA.connect(buttonA.getId(), ConstraintSet.END, buttonB.getId(), ConstraintSet.START, 0);
-        csButtonA.connect(buttonA.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
-        csButtonA.connect(buttonA.getId(), ConstraintSet.BOTTOM, buttonD.getId(), ConstraintSet.TOP, 0);
-
-        csButtonA.applyTo(myCl);
-
-        ConstraintSet csButtonB = new ConstraintSet();
-
-        csButtonB.constrainHeight(buttonB.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonB.constrainWidth(buttonB.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonB.connect(buttonB.getId(), ConstraintSet.START, buttonA.getId(), ConstraintSet.END, 0);
-        csButtonB.connect(buttonB.getId(), ConstraintSet.END, buttonC.getId(), ConstraintSet.START, 0);
-        csButtonB.connect(buttonB.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
-        csButtonB.connect(buttonB.getId(), ConstraintSet.BOTTOM, buttonE.getId(), ConstraintSet.TOP, 0);
-
-        csButtonB.applyTo(myCl);
-
-        ConstraintSet csButtonC = new ConstraintSet();
-
-        csButtonC.constrainHeight(buttonC.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonC.constrainWidth(buttonC.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonC.connect(buttonC.getId(), ConstraintSet.START, buttonB.getId(), ConstraintSet.END, 0);
-        csButtonC.connect(buttonC.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0);
-        csButtonC.connect(buttonC.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP, 0);
-        csButtonC.connect(buttonC.getId(), ConstraintSet.BOTTOM, buttonF.getId(), ConstraintSet.TOP, 0);
-
-        csButtonC.applyTo(myCl);
-
-        ConstraintSet csButtonD = new ConstraintSet();
-
-        csButtonD.constrainHeight(buttonD.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonD.constrainWidth(buttonD.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonD.connect(buttonD.getId(), ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START, 0);
-        csButtonD.connect(buttonD.getId(), ConstraintSet.END, buttonE.getId(), ConstraintSet.START, 0);
-        csButtonD.connect(buttonD.getId(), ConstraintSet.TOP, buttonA.getId(), ConstraintSet.BOTTOM, 0);
-        csButtonD.connect(buttonD.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0);
-
-        csButtonD.applyTo(myCl);
-
-        ConstraintSet csButtonE = new ConstraintSet();
-
-        csButtonE.constrainHeight(buttonE.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonE.constrainWidth(buttonE.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonE.connect(buttonE.getId(), ConstraintSet.START, buttonD.getId(), ConstraintSet.END, 0);
-        csButtonE.connect(buttonE.getId(), ConstraintSet.END, buttonF.getId(), ConstraintSet.START, 0);
-        csButtonE.connect(buttonE.getId(), ConstraintSet.TOP, buttonB.getId(), ConstraintSet.BOTTOM, 0);
-        csButtonE.connect(buttonE.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0);
-
-        csButtonE.applyTo(myCl);
-
-        ConstraintSet csButtonF = new ConstraintSet();
-
-        csButtonF.constrainHeight(buttonF.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonF.constrainWidth(buttonF.getId(), ConstraintSet.WRAP_CONTENT);
-        csButtonF.connect(buttonF.getId(), ConstraintSet.START, buttonE.getId(), ConstraintSet.END, 0);
-        csButtonF.connect(buttonF.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END, 0);
-        csButtonF.connect(buttonF.getId(), ConstraintSet.TOP, buttonC.getId(), ConstraintSet.BOTTOM, 0);
-        csButtonF.connect(buttonF.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM, 0);
-
-        csButtonF.applyTo(myCl);
-
         */
+    }
+
+    @Override
+    public void onClick(View v) {
+        TextView line = (TextView)findViewById(R.id.buttonPressed);
+        switch(v.getId()){
+            case(R.id.button_A):
+                line.setText("Button pressed: A");
+                break;
+            case(R.id.button_B):
+                line.setText("Button pressed: B");
+                break;
+            case(R.id.button_C):
+                line.setText("Button pressed: C");
+                break;
+            case(R.id.button_D):
+                line.setText("Button pressed: D");
+                break;
+            case(R.id.button_E):
+                line.setText("Button pressed: E");
+                break;
+            case(R.id.button_F):
+                line.setText("Button pressed: F");
+                break;
+        }
 
     }
 }
